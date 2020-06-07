6a99e8203fe93a7f0ff4416a90379c2e6cb04192
subbarao <subbarao.thirumoorthy@openet.com>
2020-06-07 22:20:20 +0800

    added basic config

---
+++ kstreamApp/src/main/java/com/rao2100/kstreamApp/AppConfig.java
@@ -0,0 +1,39 @@
+package com.rao2100.kstreamApp;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties
+@Component
+public class AppConfig {
+
+    String url;
+    String username;
+    String password;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
--- kstreamApp/src/main/java/com/rao2100/kstreamApp/KstreamAppApplication.java
+++ kstreamApp/src/main/java/com/rao2100/kstreamApp/KstreamAppApplication.java
@@ -2,6 +2,7 @@ package com.rao2100.kstreamApp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KstreamAppApplication implements CommandLineRunner {
 
 	private static Logger LOG = LoggerFactory.getLogger(KstreamAppApplication.class);
+
+	@Autowired
+	AppConfig appConfig;
 
 	public static void main(String[] args) {
 		LOG.info("STARTING THE APPLICATION");
@@ -23,7 +27,12 @@ public class KstreamAppApplication implements CommandLineRunner {
   
         for (int i = 0; i < args.length; ++i) {
             LOG.info("args[{}]: {}", i, args[i]);
-        }
+		}
+
+		System.out.println(appConfig.getUrl());
+		System.out.println(appConfig.getUsername());
+		System.out.println(appConfig.getPassword());
+		
 
 	}
 
