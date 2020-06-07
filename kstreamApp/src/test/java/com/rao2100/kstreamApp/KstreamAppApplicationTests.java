6e788fd6957455f0bc1724a5993c146acc6cd7ec
subbarao <subbarao.thirumoorthy@openet.com>
2020-06-07 21:59:48 +0800

    initial commit

---
+++ kstreamApp/src/test/java/com/rao2100/kstreamApp/KstreamAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rao2100.kstreamApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KstreamAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
